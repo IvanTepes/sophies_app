92951f7c3271ba7fcc6639392ff50e042af9b512
IvanTepes <ivan.tepes@outlook.com>
2022-05-28 00:44:44 +0000

    Added functions to check defauld color theme

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,12 +1,3 @@
-// Select the button
-const theme = document.body.classList.contains('my-class-name')
-const btn = document.querySelector("#theme-switch");
-// Listen for a click on the button
-btn.addEventListener("click", function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle("dark-theme");
-});
-
 
 // Function to change image src on logo when theme is changed
 function changeLogo() {
@@ -18,3 +9,29 @@ function changeLogo() {
       image.src = "media/logo/logo_big_blue.svg";
   }
 }
+
+
+let checkbox = document.querySelector("input[name=theme_switch]");
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.documentElement.setAttribute("data-theme", "dark");
+  checkbox.checked = true;
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+  checkbox.checked = false;
+}
+
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  var image = document.getElementById('logo');
+      image.src = "media/logo/logo-big-orange.svg";
+} else {
+  image.src = "media/logo/logo_big_blue.svg";
+}
+
+// switch theme if checkbox is engaged
+checkbox.addEventListener("change", (cb) => {
+  document.documentElement.setAttribute(
+    "data-theme",
+    cb.target.checked ? "dark" : "light"
+  );
+});
