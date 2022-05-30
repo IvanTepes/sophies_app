38b15640312767eb1086daf77ba6a4a94f024e24
IvanTepes <ivan.tepes@outlook.com>
2022-05-30 02:15:38 +0000

    Refactoring code

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,32 +1,31 @@
 
+
 // Function to change image src on logo when theme is changed
-function changeLogo() {
-  var image = document.getElementById('logo');
-  if (image.src.match("media/logo/logo_big_blue.svg")) {
-      image.src = "media/logo/logo-big-orange.svg";
+ function changeLogo() {
+  let logo = document.getElementById('js-logo');
+  if (logo.src.match("media/logo/logo_big_blue.svg")) {
+      logo.src = "media/logo/logo-big-orange.svg";
   }
   else {
-      image.src = "media/logo/logo_big_blue.svg";
+      logo.src = "media/logo/logo_big_blue.svg";
   }
 }
 
 
-let checkbox = document.querySelector("input[name=theme_switch]");
+// Check and set prefered theme and logo
+let logo = document.getElementById('js-logo');
+let checkbox = document.querySelector("input[name=js-nav-menu__theme-switch--checkbox]");
 
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   document.documentElement.setAttribute("data-theme", "dark");
   checkbox.checked = true;
+  logo.src = "media/logo/logo-big-orange.svg";
 } else {
   document.documentElement.setAttribute("data-theme", "light");
   checkbox.checked = false;
+  logo.src = "media/logo/logo_big_blue.svg";
 }
 
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  var image = document.getElementById('logo');
-      image.src = "media/logo/logo-big-orange.svg";
-} else {
-  image.src = "media/logo/logo_big_blue.svg";
-}
 
 // switch theme if checkbox is engaged
 checkbox.addEventListener("change", (cb) => {
